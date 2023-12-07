8bed02887e4b2bd1cddc4655fa4cdea9f3991841
OkadaHideki <okada@jriss.jp>
2023-12-07 14:10:20 +0900

    編集仮追加

--- nux/pages/index.vue
+++ nux/pages/index.vue
@@ -2,10 +2,13 @@
 <template>
     <Datepicker format="yyyy/MM/dd/HH/mm" model-type="yyyy-MM-dd-HH-mm" v-model="date" placeholder="日付を選択してください"></Datepicker>
     <div><textarea v-model="message" placeholder="文字を入力してください"></textarea></div>
-    <button v-on:click="add">作成</button>
+    <button v-on:click="postdata">作成</button>
     <button v-on:click="view">更新</button>
     <div v-for='item in data' :key="item.id">
-      <div>{{ item.time }} {{ item.memo }}<button v-on:click="deletedata(item.id)">削除</button></div>
+      <div>{{ item.time }} {{ item.memo }}
+        <button v-on:click="putdata(item.id)">編集</button>
+        <button v-on:click="deletedata(item.id)">削除</button>
+      </div>
     </div>
 </template>
 
@@ -18,25 +21,35 @@ const date = ref(new Date())
 const message = ref()
 let {data} = reactive(await useFetch('http://localhost:3200/posts'))
 
-async function add() {
+async function postdata() {
   try {
-    const response = await
-    fetch("http://localhost:3200/posts", {
+    await fetch("http://localhost:3200/posts", {
       method: "POST",
       headers: { "Content-Type": "application/json"},
       body: JSON.stringify({ time: date.value, memo: message.value})
     });
-    const result = await response.json();
     await view();
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
+async function putdata(i){
+  try{
+    await fetch('http://localhost:3200/posts/'+String(i), {
+      method: "PUT",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({ time: date.value, memo: message.value})
+    });
+    await view();
+  }catch(error){
+    console.error("Error:", error)
+  }
+}
+
 async function deletedata(i) {
   try {
-    const response = await
-    fetch('http://localhost:3200/posts/'+String(i), {
+    await fetch('http://localhost:3200/posts/'+String(i), {
         method: 'DELETE'
     })
     await view();
@@ -48,8 +61,7 @@ async function deletedata(i) {
 async function view() {
   try {
     const response = await useFetch("http://localhost:3200/posts")
-    const result = await response
-    data = await result.data
+    data = await response.data
     console.log("Success:", data)
     message.value = ''
   } catch (error) {
