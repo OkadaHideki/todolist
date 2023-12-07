96754cfda4064b2089dc6ac48c9b1169437816b0
岡田　英樹 <okada@satakehiroakinoMacBook-Air.local>
2023-12-07 10:01:48 +0900

    first

---
+++ nuxt3/nux/pages/index.vue
@@ -0,0 +1,20 @@
+<child-component @eventEmit="updateEvent" />
+<template>
+    <Datepicker v-model="date"></Datepicker>
+    <div><textarea v-model="message"></textarea></div>
+    <button>作成</button>
+    <ChildComp :deadline="date" :message="message" />
+    
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import ChildComp from './list.vue'
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref(new Date())
+const message = ref('')
+
+
+</script>
