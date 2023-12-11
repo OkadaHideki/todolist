9d7f6ba77f40531fef69b98013bb25effb38c5b2
OkadaHideki <okada@jriss.jp>
2023-12-11 16:00:22 +0900

    子構造→完了リストの表示非表示の予定

--- nux/pages/index.vue
+++ nux/pages/index.vue
@@ -13,12 +13,22 @@
         <option value="3">新規日付</option>
       </select>
     </div>
+    <div>
+      {{ donelist }}
+      <select v-model="donelist">リスト
+        <option value=0>未完了</option>
+        <option value=1>完了</option>
+      </select>
+    </div>
     <div v-for='item in data' :key="item.id">
       <div>
-        <Datepicker v-if="(new Date()).getTime() > (new Date(item.time)).getTime()" v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply dark></Datepicker>
-        <Datepicker v-else v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply></Datepicker>
-        <textarea v-model="item.memo"></textarea>
-        <button v-on:click="putdata(item.id, item.time, item.memo)">編集</button>
+        <Datepicker v-if="((new Date()).getTime() > (new Date(item.time)).getTime())" v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply dark></Datepicker>
+        <Datepicker v-else-if="((new Date()).getTime() < (new Date(item.time)).getTime())" v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply></Datepicker>
+        <input v-if="donelist" type="checkbox" v-model="item.done" v-on:click="putdata(item.id, item.time, item.memo, !item.done)"/>
+        <input v-else-if="!donelist" type="checkbox" v-model="item.done" v-on:click="putdata(item.id, item.time, item.memo, !item.done)"/>
+        <textarea v-if="donelist" v-model="item.memo"></textarea>
+        <textarea v-else-if="!donelist" v-model="item.memo"></textarea>
+        <button v-on:click="putdata(item.id, item.time, item.memo, item.done)">編集</button>
         <button v-on:click="deletedata(item.id)">削除</button>
       </div>
     </div>
@@ -33,14 +43,15 @@ let data = ref(await useFetch('http://localhost:3200/posts').data)
 const date = ref(new Date())
 const message = ref()
 const selected = ref()
-const color = ref("#f06060")
+const donelist = ref()
+let color = ref("#f06060")
 
 async function postdata() {
   try {
     await fetch("http://localhost:3200/posts", {
       method: "POST",
       headers: { "Content-Type": "application/json"},
-      body: JSON.stringify({ time: date.value, memo: message.value})
+      body: JSON.stringify({ time: date.value, memo: message.value, done: false})
     });
     await getdata();
   } catch (error) {
@@ -48,12 +59,13 @@ async function postdata() {
   }
 }
 
-async function putdata(id, time, memo){
+async function putdata(id, time, memo, done){
   try{
+    await console.log(done)
     await fetch('http://localhost:3200/posts/'+String(id), {
       method: "PUT",
       headers: { "Content-Type": "application/json"},
-      body: JSON.stringify({ time: time, memo: memo})
+      body: JSON.stringify({ time: time, memo: memo, done: done})
     });
     await getdata();
   }catch(error){
@@ -96,6 +108,13 @@ watch(selected, (i) => {
     console.log("2",data.value.sort((a, b) => (new Date(b.time)).getTime() - (new Date(a.time)).getTime()))
   }
 })
+
+watch(donelist, (i) => {
+  console.log(i)
+  i === "0" ? color.value = "#f06060" : color.value = "#ffffff"
+  console.log(donelist.value)
+})
+
 </script>
 
 <style>
@@ -122,5 +141,9 @@ watch(selected, (i) => {
     --dp-danger-color: #e53935;
     --dp-marker-color: #e53935;
     --dp-tooltip-color: #3e3e3e;
+}
+
+input[type=checkbox] {
+  transform: scale(1.5);
 }
 </style>
