04d9e18618c9875ca3889c086490f96febfcc3a3
OkadaHideki <okada@jriss.jp>
2023-12-11 12:39:27 +0900

    期限を過ぎたスケジュールの色変更

--- nux/pages/index.vue
+++ nux/pages/index.vue
@@ -15,7 +15,8 @@
     </div>
     <div v-for='item in data' :key="item.id">
       <div>
-        <Datepicker v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply></Datepicker>
+        <Datepicker v-if="(new Date()).getTime() > (new Date(item.time)).getTime()" v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply dark></Datepicker>
+        <Datepicker v-else v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply></Datepicker>
         <textarea v-model="item.memo"></textarea>
         <button v-on:click="putdata(item.id, item.time, item.memo)">編集</button>
         <button v-on:click="deletedata(item.id)">削除</button>
@@ -32,7 +33,7 @@ let data = ref(await useFetch('http://localhost:3200/posts').data)
 const date = ref(new Date())
 const message = ref()
 const selected = ref()
-const deadline = ref("dead")
+const color = ref("#f06060")
 
 async function postdata() {
   try {
@@ -96,8 +97,30 @@ watch(selected, (i) => {
   }
 })
 </script>
+
 <style>
-.dead{
-  color: red
+.dp__theme_dark {
+    --dp-background-color: v-bind(color);
+    --dp-text-color: #000000;
+    --dp-hover-color: #484848;
+    --dp-hover-text-color: #fff;
+    --dp-hover-icon-color: #959595;
+    --dp-primary-color: red;
+    --dp-primary-disabled-color: #61a8ea;
+    --dp-primary-text-color: #fff;
+    --dp-secondary-color: yellow;
+    --dp-border-color: #2d2d2d;
+    --dp-menu-border-color: red;
+    --dp-border-color-hover: #aaaeb7;
+    --dp-disabled-color: #737373;
+    --dp-disabled-color-text: #d0d0d0;
+    --dp-scroll-bar-background: #212121;
+    --dp-scroll-bar-color: #484848;
+    --dp-success-color: blue;
+    --dp-success-color-disabled: #428f59;
+    --dp-icon-color: #959595;
+    --dp-danger-color: #e53935;
+    --dp-marker-color: #e53935;
+    --dp-tooltip-color: #3e3e3e;
 }
 </style>
