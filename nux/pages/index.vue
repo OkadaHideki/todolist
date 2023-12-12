afd2f37a8ac4a512f1b831f35d724bdfe5ffb32a
OkadaHideki <okada@jriss.jp>
2023-12-12 11:08:09 +0900

    子構造→完了リストの幅表示非表示

--- nux/pages/index.vue
+++ nux/pages/index.vue
@@ -22,14 +22,7 @@
     </div>
     <div v-for='item in data' :key="item.id">
       <div>
-        <Datepicker v-if="((new Date()).getTime() > (new Date(item.time)).getTime())" v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply dark></Datepicker>
-        <Datepicker v-else-if="((new Date()).getTime() < (new Date(item.time)).getTime())" v-model="item.time" format="yyyy/MM/dd HH:mm" auto-apply></Datepicker>
-        <input v-if="donelist" type="checkbox" v-model="item.done" v-on:click="putdata(item.id, item.time, item.memo, !item.done)"/>
-        <input v-else-if="!donelist" type="checkbox" v-model="item.done" v-on:click="putdata(item.id, item.time, item.memo, !item.done)"/>
-        <textarea v-if="donelist" v-model="item.memo"></textarea>
-        <textarea v-else-if="!donelist" v-model="item.memo"></textarea>
-        <button v-on:click="putdata(item.id, item.time, item.memo, item.done)">編集</button>
-        <button v-on:click="deletedata(item.id)">削除</button>
+        <ListComp v-if="donelist==item.done" :data="item" @putdata="putdata" @deletedata="deletedata"/>
       </div>
     </div>
 </template>
@@ -38,6 +31,7 @@
 import {ref, watch} from 'vue'
 import Datepicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
+import ListComp from './list.vue'
 
 let data = ref(await useFetch('http://localhost:3200/posts').data)
 const date = ref(new Date())
@@ -45,6 +39,7 @@ const message = ref()
 const selected = ref()
 const donelist = ref()
 let color = ref("#f06060")
+
 
 async function postdata() {
   try {
@@ -96,23 +91,21 @@ async function getdata() {
 
 watch(selected, (i) => {
   if(i == 0){
-    console.log("0",data.value.sort((a, b) => a.id - b.id))
+    data.value.sort((a, b) => a.id - b.id)
   }
   else if(i == 1){
-    console.log("1",data.value.sort((a, b) => b.id - a.id))
+    data.value.sort((a, b) => b.id - a.id)
   }
   else if(i == 2){
-    console.log("2",data.value.sort((a, b) => (new Date(a.time)).getTime() - (new Date(b.time)).getTime()))
+    data.value.sort((a, b) => (new Date(a.time)).getTime() - (new Date(b.time)).getTime())
   }
   else if(i == 3){
-    console.log("2",data.value.sort((a, b) => (new Date(b.time)).getTime() - (new Date(a.time)).getTime()))
+    data.value.sort((a, b) => (new Date(b.time)).getTime() - (new Date(a.time)).getTime())
   }
 })
 
 watch(donelist, (i) => {
-  console.log(i)
   i === "0" ? color.value = "#f06060" : color.value = "#ffffff"
-  console.log(donelist.value)
 })
 
 </script>
--- nux/pages/list.vue
+++ nux/pages/list.vue
@@ -1,9 +1,27 @@
 
 
 <template>
- 
+  <div>
+    <Datepicker v-if="((new Date()).getTime() > (new Date(item.data.time)).getTime())" v-model="item.data.time" format="yyyy/MM/dd HH:mm" auto-apply dark></Datepicker>
+    <Datepicker v-else-if="((new Date()).getTime() < (new Date(item.data.time)).getTime())" v-model="item.data.time" format="yyyy/MM/dd HH:mm" auto-apply></Datepicker>
+    <input type="checkbox" v-model="item.data.done" v-on:click="emit('putdata', item.data.id, item.data.time, item.data.memo, !item.data.done)"/>
+    <textarea v-model="item.data.memo"></textarea>
+    <button v-on:click="emit('putdata', item.data.id, item.data.time, item.data.memo, item.data.done)">編集</button>
+    <button v-on:click="emit('deletedata', item.data.id)">削除</button>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const item = defineProps({
+  data: Object
+})
+
+const emit = defineEmits(['putdata', 'deletedata'])
+
+console.log(item.data)
 
 </script>
